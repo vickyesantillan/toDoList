9ed5930a21fbe9869dd483966659654ad7d2dc1d
vickyesantillan <vesantillan@live.com>
2021-06-04 17:00:54 -0300

    Making the to do list work!

--- app.js
+++ app.js
@@ -1,41 +1,58 @@
 const express = require('express');
 const app = express();
+var chores = ['Buy food', 'Cook Food', 'Eat Food'];
 
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   var today = new Date();
   var currentDay = today.getDate();
-  var day = '';
 
-  switch (currentDay) {
-    case 0:
-      day = 'Monday';
-      break;
-    case 1:
-      day = 'Tuesday';
-      break;
-    case 2:
-      day = 'Wednesday';
-      break;
-    case 3:
-      day = 'Thursday';
-      break;
-    case 4:
-      day = 'Friday';
-      break;
-    case 5:
-      day: 'Saturday';
-      break;
-    case 6:
-      day = 'Sunday';
-      break;
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
 
-    default:
-      console.log('Error: current day is equal to: ' + currentDay);
-      break;
-  }
-  res.render('list', { kindOfDay: day });
+  var day = today.toLocaleDateString('en-US', options);
+
+  // switch (currentDay) {
+  //   case 0:
+  //     day = 'Monday';
+  //     break;
+  //   case 1:
+  //     day = 'Tuesday';
+  //     break;
+  //   case 2:
+  //     day = 'Wednesday';
+  //     break;
+  //   case 3:
+  //     day = 'Thursday';
+  //     break;
+  //   case 4:
+  //     day = 'Friday';
+  //     break;
+  //   case 5:
+  //     day: 'Saturday';
+  //     break;
+  //   case 6:
+  //     day = 'Sunday';
+  //     break;
+
+  //   default:
+  //     console.log('Error: current day is equal to: ' + currentDay);
+  //     break;
+  // }
+
+  res.render('list', { kindOfDay: day, newChore: chores });
+});
+
+app.post('/', function (req, res) {
+  var chore = req.body.chore;
+  chores.push(chore);
+  res.redirect('/');
 });
 
 app.listen(3000, function () {
