cf7dbcd3f02a09511817f3699dc76f0769507322
vickyesantillan <vesantillan@live.com>
2021-06-07 16:41:15 -0300

    adding css style to the webpage

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
   let today = new Date();
   let currentDay = today.getDate();
